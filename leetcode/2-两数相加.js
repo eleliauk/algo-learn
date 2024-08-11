39ff2f9bdeed9a1a6e6ad509fb8ca223ed8add89
eleliauk <2831336720@qq.com>
2024-08-11 19:45:27 +0800

    feat:新增刷题日记和leetcode-2

---
+++ leetcode/2-两数相加.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+// 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。
+
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+// 输入：l1 = [2,4,3], l2 = [5,6,4]
+// 输出：[7,0,8]
+// 解释：342 + 465 = 807.
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var addTwoNumbers = function(l1, l2) {
+    // let val1=l1.val
+    // let val2=l2.val2
+    let sum=new ListNode("0")
+    // sum的头保留下来
+    let head=sum
+    //jinwei
+    let addOne=0
+    while (addOne || l1 || l2) {
+        let val1 = l1 !== null ? l1.val : 0 
+        let val2 = l2 !== null ? l2.val : 0 
+        let r1 = val1 + val2 + addOne
+        addOne = r1 >= 10 ? 1 : 0
+        sum.next = new ListNode(r1 % 10)
+        sum = sum.next 
+        if (l1) l1 = l1.next 
+        if (l2) l2 = l2.next 
+    }
+    return sum.next
+};
