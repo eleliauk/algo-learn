4fb0c152a3e515d66e689a1ed029fc3df20aca79
eleliauk <2831336720@qq.com>
2024-10-20 02:03:11 +0800

    feat:Array

---
+++ labuladong/lesson-0/Array/ 26-删除有序数组中的重复项.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if(nums.length===0){
+        return 0;
+    }
+    var slow=0 ,fast=0;
+    while(fast<nums.length){
+        if(nums[fast]!=nums[slow]){
+            slow++
+            nums[slow]=nums[fast];
+        }
+        fast++
+    }
+    return slow+1
+};
