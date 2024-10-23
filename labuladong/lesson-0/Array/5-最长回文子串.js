aa7f4836adf0eaf9160d9107b767fac1bd70f110
eleliauk <2831336720@qq.com>
2024-10-23 17:29:21 +0800

    feat:window

--- labuladong/lesson-0/Array/5-最长回文子串.js
+++ labuladong/lesson-0/Array/5-最长回文子串.js
@@ -2,8 +2,29 @@
  * @param {string} s
  * @return {string}
  */
+//不会 等着二进宫吧
 var longestPalindrome = function(s) {
-    var right=0;
-    var left =s.length-1;
+    var res = "";
+    var palindrome = function(s, l, r) {
+        // 在 s 中寻找以 s[l] 和 s[r] 为中心的最长回文串
+        // 防止索引越界
+        while (l >= 0 && r < s.length
+                && s.charAt(l) == s.charAt(r)) {
+            // 双指针，向两边展开
+            l--; r++;
+        }
+        // 返回以 s[l] 和 s[r] 为中心的最长回文串
+        return s.substring(l + 1, r);
+    }
+    for (var i = 0; i < s.length; i++) {
+        // 以 s[i] 为中心的最长回文子串
+        var s1 = palindrome(s, i, i);
+        // 以 s[i] 和 s[i+1] 为中心的最长回文子串
+        var s2 = palindrome(s, i, i + 1);
+        // res = longest(res, s1, s2)
+        res = res.length > s1.length ? res : s1;
+        res = res.length > s2.length ? res : s2;
+    }
+    return res;
     
 };
---
+++ labuladong/lesson-0/window/7-最小覆盖子串.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+    let need={},window={};
+    for (let c of t) {
+        need[c] = (need[c] || 0) + 1;
+    }
+    let left = 0, right = 0;
+    let valid = 0;
+    // 记录最小覆盖子串的起始索引及长度
+    let start = 0, len = Infinity;
+    while(right<s.length){
+        let c=s[right];
+        right++;
+        if(need[c]){
+            window[c] = (window[c] || 0) + 1;
+            if (window[c] === need[c]) {
+                valid++;
+            }
+        }
+        while(valid===Object.keys(needs).length){
+               // 在这里更新最小覆盖子串
+               if (right - left < len) {
+                start = left;
+                len = right - left;
+            }
+            // d 是将移出窗口的字符
+            let d = s[left];
+            // 缩小窗口
+            left++;
+            // 进行窗口内数据的一系列更新
+            if (need[d]) {
+                if (window[d] === need[d]) {
+                    valid--;
+                }
+                window[d]--;
+            }
+        }
+    }
+    // 返回最小覆盖子串
+    return len === Infinity ? "" : s.substring(start, start + len);
+};
